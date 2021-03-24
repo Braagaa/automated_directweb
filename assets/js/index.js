9b05e89779cfdee00b50c87d7cdf5fae203073a2
braagaa <michael_braga@outlook.com>
2021-03-24 17:12:32 -0400

    add await to request

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@
   registerButton.addEventListener("click", async () => {
     try {
       //user.id
-      const token = fetch("/token", {
+      const token = await fetch("/token", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, type }),
